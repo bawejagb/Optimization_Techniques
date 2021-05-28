ee42051acdd9f39410ecdba545b4e3c0c02ff797
Gaurav <gaurav.baweja2508@gmail.com>
2021-05-28 15:51:10 +0530

    Dual Simplex added

--- 2_Phase_Method.m
+++ 2_Phase_Method.m
@@ -1,15 +1,22 @@
 %2 Phase Method
 clc
 clear
-%format rat
+format rat
 %Equations 
-A = [1 3 1; 2 -1 1; 4 3 -2];                                
-B = [5; 2; 5];
+A = [1 3; 1 1];                                
+B = [3; 2];
 %Objective function
-C=[3 -1 2];
+C=[3 5];
+min1_max0 = 1; % Min/Max -> 1/0
+if(min1_max0 == 1)
+    C = -C;
+end
 %Phase 1
-c1 = [0 0 0 0 0 -1 0];
-S = [A [1 0 0 0; 0 -1 1 0; 0 0 0 1]];
+fprintf("-------------- PHASE 1 --------------\n");
+c1 = [0 0 0 0 -1 -1]; %COST
+c1 = [c1 0];% with solution
+Art_var = find(c1 == -1);
+S = [A [-1 0 1 0; 0 -1 0 1] B];
 %Identify Basic variable
 Idy = eye(size(S,1));
 Bv = [];
@@ -22,11 +29,9 @@ for i=1:size(S,2)
 end
 %Finding ZjCj for Table
 ZjCj = c1(Bv)*S - c1;
-S = [S B];
-ZjCj = [ZjCj 0]; % ZjCj with solution
 %Finding Optimal table for Maximization problem
-while(any(ZjCj < 0))
-    [minVal, PvtCol] = min(ZjCj);
+while(any(ZjCj(1:end-1) < 0))
+    [minVal, PvtCol] = min(ZjCj(1:end-1));
     for i = 1: size(S,1)
        if S(i,PvtCol) > 0
           ratio(i) =  S(i,size(S,2))/S(i,PvtCol);
@@ -35,6 +40,7 @@ while(any(ZjCj < 0))
        end
     end
     [minratio, PvtRow] = min(ratio);
+    Bv(PvtRow) = PvtCol;
     PvtElement = S(PvtRow, PvtCol);
     S(PvtRow,:) = S(PvtRow,:) / PvtElement;
     for i = 1:size(S,1)
@@ -42,33 +48,60 @@ while(any(ZjCj < 0))
           S(i,:) = S(i,:) - S(i,PvtCol)*S(PvtRow,:);  
        end
     end
-    ZjCj = ZjCj - ZjCj(PvtCol)*S(PvtRow,:);
+    %ZjCj = ZjCj - ZjCj(PvtCol)*S(PvtRow,:)
+    ZjCj = c1(Bv)*S - c1;
+    
 end
-OptimalTable1 = array2table(S,'variableNames',{'x1','x2','x3','s1','s2','a1','s3','B'})
-%fprintf("Solution: %d\n",ZjCj(size(ZjCj,2)));
-%Phase 2
-CZ = zeros(size(A,1)+1);
-c2 = [C CZ(1,:)];
-ZjCj = [-c2 0]; % ZjCj with solution
-%Finding Optimal table for Maximization problem
-while(any(ZjCj < 0))
-    [minVal, PvtCol] = min(ZjCj);
-    for i = 1: size(S,1)
-       if S(i,PvtCol) > 0
-          ratio(i) =  S(i,size(S,2))/S(i,PvtCol);
-       else
-           ratio(i) = inf;
-       end
+OptimalTable1 = array2table(S,'variableNames',{'x1','x2','s1','s2','a1','a2','B'})
+ZjCj
+%fprintf("Solution: %d\n",ZjCj(end));
+feasible_sol_exist = 1;
+for i = 1:size(Bv,1)
+    if any(Bv(i) == Art_var)
+        feasible_sol_exist = 0;
+        fprintf("Infeasible Solution\n");
+        break;
     end
-    [minratio, PvtRow] = min(ratio);
-    PvtElement = S(PvtRow, PvtCol);
-    S(PvtRow,:) = S(PvtRow,:) / PvtElement;
-    for i = 1:size(S,1)
-       if i ~= PvtRow
-          S(i,:) = S(i,:) - S(i,PvtCol)*S(PvtRow,:);  
-       end
-    end
-    ZjCj = ZjCj - ZjCj(PvtCol)*S(PvtRow,:);
 end
-OptimalTable2 = array2table(S,'variableNames',{'x1','x2','x3','s1','s2','a1','s3','B'})
-fprintf("Solution: %d\n",ZjCj(size(ZjCj,2)));
+if feasible_sol_exist==1
+    %Phase 2
+    fprintf("-------------- PHASE 2 --------------\n");
+    c2 = [C [0 0 0]];
+    S(:,Art_var) = []; % Remove Artificial variable
+    ZjCj = c2(Bv)*S - c2; % ZjCj with solution
+    %Finding Optimal table for Maximization problem
+    while(any(ZjCj(1:end-1) < 0))
+        [minVal, PvtCol] = min(ZjCj(1:end-1));
+        if all(S(:,PvtCol)<=0)
+            error= fprintf('LPP is unbounded\n');
+            feasible_sol_exist = 0;
+            break;
+        end
+        for i = 1: size(S,1)
+           if S(i,PvtCol) > 0
+              ratio(i) =  S(i,size(S,2))/S(i,PvtCol);
+           else
+               ratio(i) = inf;
+           end
+        end
+        [minratio, PvtRow] = min(ratio);
+        Bv(PvtRow) = PvtCol;
+        PvtElement = S(PvtRow, PvtCol);
+        S(PvtRow,:) = S(PvtRow,:) / PvtElement;
+        for i = 1:size(S,1)
+           if i ~= PvtRow
+              S(i,:) = S(i,:) - S(i,PvtCol)*S(PvtRow,:);  
+           end
+        end
+        %ZjCj = ZjCj - ZjCj(PvtCol)*S(PvtRow,:);
+        ZjCj = c2(Bv)*S - c2;
+    end
+    if (feasible_sol_exist==1)
+        OptimalTable2 = array2table(S,'variableNames',{'x1','x2','s1','s2','B'})
+        optimal_solution = ZjCj(end);
+        if(min1_max0 == 1)
+            optimal_solution = -optimal_solution;
+        end
+        fprintf("Solution: %d\n",optimal_solution);
+    end
+end
---
+++ Dual_Simplex_Method.m
@@ -0,0 +1,36 @@
+%Dual-Simplex Method
+clc
+clear all
+cost = [-2  0 -1 0 0 0];
+A = [-1 -1 1; -1 2 -4];
+B =[-5; -8];
+s = eye(size(A,1));
+S = [A s B];
+bv = [4 5];
+ZjCj = cost(bv)*S-cost;
+sol = S(:,end);
+while any (sol < 0)
+    [minval, piv_row] = min(sol);
+    row_val = S(piv_row, 1:end-1);
+    for i = 1:size(S,2)-1
+        if(row_val(i) < 0)
+            ratio(i) = abs(ZjCj(i)/row_val(i));
+        else
+            ratio(i) = inf;
+        end
+    end
+    [ent_val, piv_col] = min(ratio);
+    piv_key = S(piv_row, piv_col);
+    S(piv_row,:) = S(piv_row,:)/piv_key;
+    bv(piv_row) = piv_col;
+    for(i = 1:size(S,1))
+        if( i~= piv_row)
+            S(i,:) = S(i,:) - S(i,piv_col)*S(piv_row,:);
+        end
+    end
+    ZjCj = cost(bv)*S-cost;
+    sol = S(:,end);
+end
+OptimalTable = array2table(S,'variableNames',{'x1','x2','x3','s1','s2''s3','B'})
+optimal_solution = ZjCj(end);
+fprintf("Solution: %d\n",optimal_solution);
