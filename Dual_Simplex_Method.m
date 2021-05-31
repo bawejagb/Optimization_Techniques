353ba37b5f68665351cb613ed274625a55a5d98d
Gaurav <gaurav.baweja2508@gmail.com>
2021-05-31 23:18:09 +0530

    updated for min_max

--- Dual_Simplex_Method.m
+++ Dual_Simplex_Method.m
@@ -2,6 +2,10 @@
 clc
 clear all
 cost = [-2  0 -1 0 0 0];
+min1_max0 = 0; % Min/Max -> 1/0
+if(min1_max0 == 1)
+    cost = -cost;
+end
 A = [-1 -1 1; -1 2 -4];
 B =[-5; -8];
 s = eye(size(A,1));
@@ -33,4 +37,7 @@ while any (sol < 0)
 end
 OptimalTable = array2table(S,'variableNames',{'x1','x2','x3','s1','s2''s3','B'})
 optimal_solution = ZjCj(end);
+if(min1_max0 == 1)
+    optimal_solution = -optimal_solution;
+end
 fprintf("Solution: %d\n",optimal_solution);
